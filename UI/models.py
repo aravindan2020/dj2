4521058fa0da9446ece112d2ea64bb352432d040
aravindan2020 <kkl.aravind@gmail.com>
2021-02-11 21:15:26 +0530

    model added

--- UI/models.py
+++ UI/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class cl1:
+    name : str
+    img : str
+    desc :str
--- UI/views.py
+++ UI/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import cl1
 
 # Create your views here.
 
 def index(request):
-     return render(request,'index.html')
+     c1=cl1()
+     c1.name='titanic hero'
+     c1.desc='sell me this pen'
+     c1.img='11.jpg'
+
+     c2=cl1()
+     c2.name='iron man'
+     c2.desc='i am iron man'
+     c2.img='12.jpeg'
+
+     c3=cl1()
+     c3.name='batman'
+     c3.desc='i am vengence'
+     c3.img='13.jpeg'
+
+     d=[c1,c2,c3]   
+
+     return render(request,'index.html',{'d':d})
